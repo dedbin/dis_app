8c8b689e501691ddb14ceab95edd449fc55c9864
dedbin <black13dog@yandex.ru>
2023-09-30 13:30:20 +0300

    add simple uploadthing api

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -26,6 +26,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
+import { FileUpload } from "../file-upload";
   const formSchema = z.object({
       name: z.string().min(1, { 
         message: "Server name is required" 
@@ -73,7 +74,21 @@ export const InitialModal = () => {
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                             <div className="space-y-8 px-6">
                                 <div className="flex items-center justify-center text-center">
-                                    TODO: Add logo upload
+                                    <FormField
+                                    control={form.control}
+                                    name="logoUrl"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                    />
                                 </div>
                                 <FormField
                                     control={form.control}
