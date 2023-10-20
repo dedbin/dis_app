68b38633949b03630081cb6ed925ef8f21c54e8b
dedbin <black13dog@yandex.ru>
2023-10-20 17:54:16 +0300

    strart creating chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -34,6 +35,15 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
             serverId={params.serverId} 
             name={channel.name} 
             type="channel"
+        />
+        <div className="flex-1">
+            future message
+        </div>
+        <ChatInput
+          name={channel.name}
+          type="channel"
+          apiUrl="/api/socket/messages"
+          query={{channelId: channel.id, serverId: channel.serverId}}
         />
     </div> );
 }
---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import axios from "axios";
+import qs from "query-string";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
+import { Input } from "../ui/input";
+import { Plus, Smile } from "lucide-react";
+
+interface ChatInputProps {
+    apiUrl: string;
+    query: Record<string, any>;
+    name: string;
+    type: 'channel' | 'conversation';
+}
+
+const formSchema = z.object({
+    content: z.string().min(1),
+})
+
+export const ChatInput = ({apiUrl, query, name, type}: ChatInputProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {content: "",}
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try{
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query,
+            });
+            await axios.post(url, values);
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    const isLoading = form.formState.isSubmitting;
+
+    return (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="relative p-4 pb-4">
+                      <button
+                        type="button"
+                        onClick={()=>{}}
+                        className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400
+                         hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                      >
+                        <Plus className="text-white dark:text-[#2b2d33]"/>
+                      </button>
+                      <Input
+                        disabled={isLoading}
+                        placeholder={`Message ${type === 'conversation' ? name: '#' + name}`}
+                        className="px-14 py-6 text-zinc-600 dark:text-zinc-300 focus-visible:ring-offset-0
+                         focus-visible:ring-0 border-none border-0 bg-zinc-200 dark:bg-zinc-700"
+                        {...field}
+                      />
+                      <div className="absolute top-7 right-8">
+                        <Smile/>
+                      </div>
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+    );
+}
